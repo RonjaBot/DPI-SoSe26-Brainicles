f3b12b7b47ff7862bfcab9b5f7a8472814ae0e42
Vi-Cookie <vidao0706@gmail.com>
2026-06-12 17:40:05 +0200

    Add row count for schm_behandlungen table

--- src/W8/Schmidt/02_row_statistics_schm.sql
+++ src/W8/Schmidt/02_row_statistics_schm.sql
@@ -1,4 +1,11 @@
 SELECT
     'staging.schm_kunden' AS tabelle,
     COUNT(*) AS zeilen
-FROM staging.schm_kunden;
+FROM staging.schm_kunden
+    
+UNION ALL
+
+SELECT
+     'staging.schm_behandlungen' AS tabelle,
+     COUNT(*) AS zeilen
+FROM staging.schm_behandlungen;
