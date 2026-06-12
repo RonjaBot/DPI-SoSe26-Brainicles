dd0472e7cc25a90a0868438faf8982876095ccf6
Vi-Cookie <vidao0706@gmail.com>
2026-06-12 17:35:33 +0200

    Add staging tables for Kunden and Behandlungen

--- src/W8/Schmidt/01_extract_staging_schm.sql
+++ src/W8/Schmidt/01_extract_staging_schm.sql
@@ -1,4 +1,5 @@
 -- Schmidt Kunden
+
 DROP TABLE IF EXISTS staging.schm_kunden;
 
 CREATE TABLE staging.schm_kunden AS
@@ -9,3 +10,39 @@ CREATE TABLE staging.schm_kunden AS
              header=true,
              all_varchar=true
          );
+
+-- Schmidt Behandlungen
+
+DROP TABLE IF EXISTS staging.schm_behandlungen_unflat;
+CREATE TABLE staging.schm_behandlungen_unflat AS
+
+         SELECT *
+
+         FROM read_json_auto(
+
+             'C:/Users/vidao/fallstudie_datenmanagement_master/verbund/praxis_schmidt_behandlungen.json',
+
+             format='auto'
+
+         );
+
+DROP TABLE IF EXISTS staging.schm_behandlungen;
+CREATE TABLE staging.schm_behandlungen AS 
+
+SELECT
+
+    id,
+
+    datum,
+
+    kunde,
+
+    tier.name AS tier_name,
+
+    tier.art AS tier_art,
+
+    leistung,
+
+    betrag
+
+FROM staging.schm_behandlungen_unflat;
